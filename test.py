f710e4eb7e0768a8c360238e7c8a1e5ffa6746e0
MartynasArba <martynas.arbaciauskas@gmail.com>
2024-09-18 18:12:10 +0300

    third week

---
+++ test.py
@@ -0,0 +1,10 @@
+print('ivesk skaiciu')
+skaicius = input()
+skaicius = int(skaicius)
+
+if skaicius > 5:
+    print('skaicius didesnis nei 5')
+elif skaicius == 5:
+    print('skaicius lygus 5')
+else:
+    print('skaicius mazesnis nei 5')
